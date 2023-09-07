f5a718c2c3181a90becea20fcda0c2d8a2f5266e
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-09-07 12:19:03 +0200

    added files

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const currentUTCTimeElement = document.getElementById("currentUTCTime");
+  const currentDayOfTheWeekElement = document.getElementById(
+    "currentDayOfTheWeek"
+  );
+
+  function updateDateTime() {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.toISOString(); // Use ISOString to include milliseconds
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+
+    currentUTCTimeElement.textContent = currentUTCTime;
+    currentDayOfTheWeekElement.textContent = currentDayOfWeek;
+  }
+
+  setInterval(updateDateTime, 1000);
+  updateDateTime();
+});
